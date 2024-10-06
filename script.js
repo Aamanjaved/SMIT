7148385913c0ed8de7d092b73e89a94a6ebb17b7
Aamanjaved <aamanad677@gmail.com>
2024-10-06 16:22:41 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,48 @@
+// var word = prompt("Enter Word");
+// function palindromeChecker(reverseWord) {
+//     var normalWord = recievewords.spilt("").reverse().join("");
+//     return normalWord === recievewords ? true : false
+// }
+// alert (palindromeChecker(word));
+
+
+// var userPrompt = prompt("Enter Number Between 1,7");
+
+// if (userPrompt == 1) {
+//     alert("Monday")
+// }else if (userPrompt == 2) {
+//     alert("Tuesday")
+// }else if (userPrompt == 3) {
+//     alert("Wednesday")
+// }else if (userPrompt == 4) {
+//     alert("Thursday")
+// }else if (userPrompt == 5) {
+//     alert("Friday")
+// }else if (userPrompt == 6) {
+//     alert("Saturday")
+// }else if(userPrompt == 7) {
+//     alert("Sunday")
+// };
+
+
+// var word = "This is my book";
+// var splitWord = word.split(' ');
+// var reverseWord = "";
+
+// for(var i = 0; i < splitWord.length; i++){
+//     reverseWord += splitWord[i].split("").reverse().join("") + " ";
+// }
+
+// console.log(reverseWord);
+
+  
+// var correctNumber = 29;
+// var guessNumber = 23;
+
+// while(correctNumber !== guessNumber){
+//     guessNumber = Number(prompt("Enter Number"));
+//     if (correctNumber !== guessNumber) {
+//         alert("Try again")
+//     }
+// }   
+// alert("You Win");
